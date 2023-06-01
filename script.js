8bbebe70600e0065ea4435536471ac1f2682eb9e
Pranay Desai <pdesai22@stevens.edu>
2023-06-01 12:36:13 -0400

    render logic clean up and add div for display of moves

--- script.js
+++ script.js
@@ -73,33 +73,10 @@ const setUpBoard = () => {
   startPosition.forEach((chessMan, i) => {
     const square = document.createElement("div");
     square.classList.add("square");
-
-    if (i < 8) {
-      countFile = ((i + 1) % 8 !== 0 ? (i + 1) % 8 : 8) - 1;
-      square.setAttribute("id", `${ranks[7]}-${files[countFile]}`);
-    } else if (i >= 8 && i < 16) {
-      countFile = ((i + 1) % 8 !== 0 ? (i + 1) % 8 : 8) - 1;
-      square.setAttribute("id", `${ranks[6]}-${files[countFile]}`);
-    } else if (i >= 16 && i < 24) {
-      countFile = ((i + 1) % 8 !== 0 ? (i + 1) % 8 : 8) - 1;
-      square.setAttribute("id", `${ranks[5]}-${files[countFile]}`);
-    } else if (i >= 24 && i < 32) {
-      countFile = ((i + 1) % 8 !== 0 ? (i + 1) % 8 : 8) - 1;
-      square.setAttribute("id", `${ranks[4]}-${files[countFile]}`);
-    } else if (i >= 32 && i < 40) {
-      countFile = ((i + 1) % 8 !== 0 ? (i + 1) % 8 : 8) - 1;
-      square.setAttribute("id", `${ranks[3]}-${files[countFile]}`);
-    } else if (i >= 40 && i < 48) {
-      countFile = ((i + 1) % 8 !== 0 ? (i + 1) % 8 : 8) - 1;
-      square.setAttribute("id", `${ranks[2]}-${files[countFile]}`);
-    } else if (i >= 48 && i < 56) {
-      countFile = ((i + 1) % 8 !== 0 ? (i + 1) % 8 : 8) - 1;
-      square.setAttribute("id", `${ranks[1]}-${files[countFile]}`);
-    } else if (i >= 56 && i < 64) {
-      countFile = ((i + 1) % 8 !== 0 ? (i + 1) % 8 : 8) - 1;
-      square.setAttribute("id", `${ranks[0]}-${files[countFile]}`);
-    }
     row = Math.floor((63 - i) / 8) + 1;
+    countFile = ((i + 1) % 8 !== 0 ? (i + 1) % 8 : 8) - 1;
+    square.setAttribute("id", `${ranks[row - 1]}-${files[countFile]}`);
+    console.log(row);
     if (row % 2 === 0) {
       square.classList.add(i % 2 === 0 ? "bgSkin" : "bgGreen");
     } else {
